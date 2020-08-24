8bc973e87749247cbeaa67e2dae61927691aad9a
PIMOOK KITTILADAKUL <63010692@KMITL.AC.TH>
2020-08-24 15:24:17 +0700

    Add project files.

---
+++ 031/Source.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main () {
+	int a;
+	scanf_s("%d", &a);
+	if (a < 0) {
+		printf("Please insert the number that is greater or equal zero");
+	}
+	else if (a >= 0) {
+		if (a >= 50) {
+			printf("Pass");
+		}
+		else {
+			printf("Fail");
+		}
+	}
+
+}
